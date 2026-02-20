e4e5f90365225884d1936dfea7af09a7f541d89c
Priscila Oliveira <164383318+PriscillaOliveiraa@users.noreply.github.com>
2026-02-20 16:04:21 -0300

    Update Aula16-Tuplas.py

--- Aulas/Aula16-Tuplas.py
+++ Aulas/Aula16-Tuplas.py
@@ -1,5 +1,6 @@
 #lanche = ('Hamburguer', 'suco', 'pudim', 'pizza')
 lanche = ('0', '1', '2', '3', '4')
+
 #Tuplas são imutavéis
 print(lanche[1])
 print(lanche[-2])
@@ -13,14 +14,16 @@ for comida in lanche:
     print(f'Eu vou comer {comida}')
 print('Comi pra caramba!')
 print('=' * 30)
+
 for cont in range(0, len(lanche)):
     #print(lanche[cont]) ou
     print(f'Agora vou comer {lanche[cont]} na posição {cont}')
 
 print('=' * 30)
 
-'''for pos, comida in enumerate(lanche):
+for pos, comida in enumerate(lanche):
     print(f'Eu vou comer {comida} na posição {pos}')
 
 print('=' * 30)
-print(sorted(lanche))#para ordenar a lista'''
+
+print(sorted(lanche))#para ordenar a lista
